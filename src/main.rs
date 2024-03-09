55a778ca4e71a1454fabe0d9899a2993dcbf7220
MrChaker <mr.chakerw@gmail.com>
2024-03-09 14:57:17 +0100

    Save result in file and format it

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,75 @@
 pub mod element_text;
 pub mod transform;
-
 use leptos::html::Tr;
+use std::fs::File;
+use std::io::prelude::*;
+use std::process::Command;
 use transform::Transformer;
 
 fn main() {
     let transformer: Transformer = Transformer::new(
         vec!["img", "SectionDivider"],
-        vec!["Paragraphs", "Example"],
+        vec!["Paragraphs", "Example", "Section", "Solution"],
+        vec!["Example"],
         "Paragraph",
+        vec![
+            "Image",
+            "DisplayImage",
+            "Pause",
+            "InlineImage",
+            "MathBlock",
+            "Table",
+        ],
+        vec![
+            "Paragraphs",
+            "Paragraph",
+            "Example",
+            "Section",
+            "tr",
+            "Table",
+            "Solution",
+        ],
     );
 
     let html_code = transformer.transform(
         r#"
 |> Section
 
-    |> Paragraphs
+    ok 
 
-        on both sides by $x-x_0$, we find the fraction-less equation
+    |> Table
 
-        $$
-        p(x-x_0) = y-y_0
-        $$
-
-        which is satisfied by the point $(x,y) = (x_0,y_0)$ as well as
-        by every other point on the line.
-        
-
+    Unfortunately, the equation
+            
     "#
         .to_string(),
         0,
     );
-    println!("{}", html_code);
+    let mut file = match File::create("src/output.rs") {
+        Ok(file) => file,
+        Err(error) => {
+            println!("Error creating file: {}", error);
+            return;
+        }
+    };
+    let file_content = format!(
+        r#"
+    view! {{
+        {}
+    }}
+    "#,
+        html_code
+    );
+    match file.write_all(file_content.as_bytes()) {
+        Ok(_) => {
+            println!(
+            "Data written to file successfully , use leptosfmt to format leptos for better view"
+        );
+            let _ = Command::new("leptosfmt")
+                .arg("src/output.rs")
+                .output()
+                .expect("Failed to execute command");
+        }
+        Err(error) => println!("Error writing to file: {}", error),
+    }
 }
